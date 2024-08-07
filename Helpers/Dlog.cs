84ed19bc9efa11bf6e7a9cdf11d2bf9717c563c8
Andreas Hnida <ah@andreashnida.de>
2024-08-07 23:23:42 +0200

    init

---
+++ Helpers/Dlog.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace PapaControlApp.Helpers
+{
+    public static class Dlog
+    {
+        public static bool IsEnabled { get; set; } = true;
+
+        public static void Log(string message)
+        {
+            if (IsEnabled)
+                Debug.WriteLine(message);
+        }
+    }
+}
